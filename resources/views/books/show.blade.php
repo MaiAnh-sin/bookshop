dbc9f09f7bbd8ea5570d7052611a643c1164c4b9
MaiAnh-sin <22010242@st.phenikaa-uni.edu.vn>
2025-02-27 01:32:39 +0700

    bookshop

--- resources/views/books/show.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>{{ $book->title }}</h1>
-    <p>Tác giả: {{ $book->author }}</p>
-    <p>Giá: ${{ $book->price }}</p>
-    <p>Mô tả: {{ $book->description }}</p>
-    <a href="{{ route('books.index') }}">Trở về</a>
-@endsection
