5456dbe1cab19607fdc604ce8d02733a775a05ce
MaiAnh-sin <22010242@st.phenikaa-uni.edu.vn>
2025-02-27 00:10:31 +0700

    BookShop

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,11 @@ Route::get('/', function () {
 Route::get('/user', function () {
     return view('user');
 });
+Route::get('/book', function () {
+    return view('layouts.app');
+});
 
 use App\Http\Controllers\BookController;
 
-Route::resource('books', BookController::class);
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
